fd99000f9de002cfa9278dc17c41f84382ba9e06
Anton Rashchenko <tonraschenko@hotmail.com>
2020-08-21 10:16:22 +0200

    first commit

---
+++ Log4net.Test.Application/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
+namespace Log4net.Test.Application
+{
+    class Program
+    {
+        //log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private static readonly log4net.ILog log = LogHelper.GetLogger();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            log.Error("This is my error message");
+
+            Console.ReadLine();
+        }
+    }
+}
